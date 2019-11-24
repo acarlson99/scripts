d7b985e22bb5d97098b22958c0ea2bf7d0f774e3
Alexander <bombblob@gmail.com>
2019-11-23 17:09:19 -0800

    Added spotify script

---
+++ spotify/spotify-playlist-to-file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+	echo "usage: $0 playlisturl"
+	exit 1
+fi
+
+OUT=/tmp/spotify-tmp-out.html
+curl $1 > $OUT
+NAME=""
+BAND=""
+while read LINE; do
+	if [[ $LINE == *'class="track-name"'* ]]; then
+		echo $LINE | sed -E 's/<\/div><\/div><div class="tracklist-col name"><div class="top-align track-name-wrapper"><span class="track-name" dir="auto">(.*)<\/span><span class="artists-albums"><a href="\/[[:alnum:]]+\/[[:alnum:]]+" tabindex="-1"><span dir="auto">([^<>]+)<.*$/\1 \2/'
+	fi
+done < $OUT
+
+rm $OUT
